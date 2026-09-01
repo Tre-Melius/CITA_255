fe51a6b281e799c898c4556c048c7e6371265cc7
Tre-Melius <melius569@morrisville.edu>
2026-09-01 17:31:53 -0400

    adding project

---
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Ming Imperial.ttf", "Ming Imperial");
+                    fonts.AddFont("Pierce Jameson Script.otf", "Pierce Jameson Script");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
